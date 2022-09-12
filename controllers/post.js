0a63e6f483e52131bd5db27fb30640484d0891f4
Doppler <thomas.bertin@yahoo.fr>
2022-09-12 18:26:04 +0200

    routes to get all posts, to get one post and to delete one post are now working

--- controllers/post.js
+++ controllers/post.js
@@ -21,77 +21,49 @@ exports.createPost = (req, res, next) => {
   });
 };  
 
-// Bellow there s only code from P6 working with Mongo Db :
-
 exports.getAllPost = (req, res, next) => {
-  Post.find().then(
-    (posts) => {
-      res.status(200).json(posts);
+  const sql =
+    "SELECT * FROM posts p, users u WHERE u.user_isActive=1 AND p.post_isActive=1 AND u.user_id=p.post_userId ORDER BY post_time DESC;";
+  db.query(sql, (err, result) => {
+    if (result){
+    res.status(200).json(result);
     }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
+    if (err) {
+      res.status(404).json({ err });
+      throw err;
     }
-  );
+  });
 };
 
 exports.getOnePost = (req, res, next) => {
-  Post.findOne({
-    _id: req.params.id
-  }).then(
-    (post) => {
-      res.status(200).json(post);
+  const { id : postId } = req.params;
+  const sql = `SELECT * FROM posts p, users u WHERE p.post_id = ${postId} AND u.user_id=p.post_userId;`;
+  db.query(sql, (err, result) => {
+    if (result){
+      res.status(200).json(result);
     }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
+    if (err) {
+      res.status(404).json({ err });
+      throw err;
     }
-  );
-};
-
-exports.modifyPost = (req, res, next) => {
-  const postObject = req.file ? {
-    ...JSON.parse(req.body.post),
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  } : { ...req.body };
-  delete postObject._userId;
-  Post.findOne({ _id: req.params.id })
-    .then((post) => {
-      if (post.userId != req.auth.userId) {
-        res.status(403).json({ message: 'Unauthorized request' });
-      } else {
-        Post.updateOne({ _id: req.params.id }, { ...postObject, _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Objet modifié!' }))
-          .catch(error => res.status(401).json({ error }));
-      }
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+  });
 };
 
 exports.deletePost = (req, res, next) => {
-  Post.findOne({ _id: req.params.id })
-    .then(post => {
-      if (post.userId != req.auth.userId) {
-        res.status(403).json({ message: 'Unauthorized request' });
-      } else {
-        const filename = post.imageUrl.split('/images/')[1];
-        fs.unlink(`images/${filename}`, () => {
-          Post.deleteOne({ _id: req.params.id })
-            .then(() => { res.status(200).json({ message: 'Objet supprimé !' }) })
-            .catch(error => res.status(401).json({ error }));
-        });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ error });
-    });
+  const { id: post_id } = req.params
+  const sql = `DELETE FROM posts WHERE post_id = ${post_id}`;
+  db.query(sql, (err, result) => {
+    if (result){
+      res.status(200).json(result);
+    }
+    if (err) {
+      res.status(404).json({ err });
+      throw err;
+    }
+  });
 };
+
+// Bellow there s only code from P6 working with Mongo Db :
 
 exports.notePost = (req, res, next) => {
   let like = req.body.like;
@@ -145,5 +117,28 @@ exports.notePost = (req, res, next) => {
       console.log(error);
   }
 }
+
+// Bellow there s only code from P6 working with Mongo Db :
+
+exports.modifyPost = (req, res, next) => {
+  const postObject = req.file ? {
+    ...JSON.parse(req.body.post),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+  delete postObject._userId;
+  Post.findOne({ _id: req.params.id })
+    .then((post) => {
+      if (post.userId != req.auth.userId) {
+        res.status(403).json({ message: 'Unauthorized request' });
+      } else {
+        Post.updateOne({ _id: req.params.id }, { ...postObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+          .catch(error => res.status(401).json({ error }));
+      }
+    })
+    .catch((error) => {
+      res.status(400).json({ error });
+    });
+};
 
 
